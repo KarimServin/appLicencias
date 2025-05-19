fb3de8d68ed4425dd4d0d2588960b4ea602137dd
KarimServin <karimservin@outlook.es>
2025-05-18 21:09:34 -0300

    primer commit

---
+++ src/main/java/com/municipalidad/licencias/appLicencias/AppLicenciasApplication.java
@@ -0,0 +1,13 @@
+package com.municipalidad.licencias.appLicencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLicenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLicenciasApplication.class, args);
+	}
+
+}
