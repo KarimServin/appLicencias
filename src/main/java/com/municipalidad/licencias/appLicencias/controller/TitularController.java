fd5853e30e6d8f8cc06b004a4da60ee1876c519c
miguechiape <miguechiape@gmail.com>
2025-06-05 10:57:37 -0300

    Actualizacion de controllers

--- src/main/java/com/municipalidad/licencias/appLicencias/controller/TitularController.java
+++ src/main/java/com/municipalidad/licencias/appLicencias/controller/TitularController.java
@@ -1,20 +1,15 @@
 package com.municipalidad.licencias.appLicencias.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+
 
 import com.municipalidad.licencias.appLicencias.model.Titular;
 import com.municipalidad.licencias.appLicencias.service.TitularService;
 
 
 
-@RestController
-@RequestMapping("/titular")
+@Controller
 public class TitularController {
     
     private final TitularService titularService;
@@ -23,14 +18,14 @@ public class TitularController {
         this.titularService = titularService;
     }
 
-    @PostMapping("/crearTitular")
-    public ResponseEntity<Titular> crearTitular(@RequestBody Titular titular) {
+    
+    public ResponseEntity<Titular> crearTitular( Titular titular) {
         Titular creado = titularService.crearTitular(titular);
         return ResponseEntity.ok(creado);
     }
 
-     @GetMapping("/{dni}")
-    public ResponseEntity<Titular> buscarTitular(@PathVariable Long dni) {
+
+    public ResponseEntity<Titular> buscarTitular( Long dni) {
         return titularService.buscarPorDni(dni)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
